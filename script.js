6b0135181e8003a4ef7fd0ad057e27aa374b1928
Isaiah G <isaiahrg12@gmail.com>
2021-11-14 07:53:37 -0700

    Modified to complete 'Assign Dog object to your profile HW'

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let dog = {
     name: "Steve",
     age: 1,
     location: "Japan",
-    profileImg: "https://images.unsplash.com/photo-1613410180211-106788bd2d94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+    profileImg: "url('https://images.unsplash.com/photo-1613410180211-106788bd2d94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80')",
     bannerImg: "url('https://thumbs.dreamstime.com/b/green-polka-dot-background-25383307.jpg')",
     backgroundColor: "#00FFFF",
     stats: {
@@ -14,40 +14,91 @@ let dog = {
     }
 }
 
+let cat = {
+    name: "Mochi",
+    age: 1,
+    location: "Philippines",
+    profileImg: "url('https://images.unsplash.com/photo-1621780030440-71c9ace78fa3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fG9yYW5nZSUyMGNhdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60')",
+    bannerImg: "url('https://www.teahub.io/photos/full/159-1595602_chevron-desktop-wallpapers-hd-blue-striped-wallpaper-hd.jpg')",
+    backgroundColor: "#FFD580",
+    stats: {
+        followers: "4.5M",
+        likes: "12M",
+        posts: "8K"
+    },
+
+    speak() {
+        return "meow meow"
+    }
+}
+
+
 // console.log(document.getElementById("full-name").style.color = "blue");
 
-//Get access to your name 
-// let dubbed = document.querySelector("#full-name").innerText = dog.name;
-// console.log(dubbed)
+// document.getElementById("#full-name").addEventListener("click", function() {
+//     document.getElementById("#full-name").style.color = "blue"
+// })
+//--------------------------------------------------------------------------------------
 
-//Get access to your age
-// let age = document.querySelector("#text1").innerText = dog.age;
+
+// Get access to your name --------------------------------------------------------------------------------------------------
+document.querySelector("#full-name").innerText = dog.name;
+// console.log(document.querySelector("#full-name").innerText)
+
+//Get access to your age ---------------------------------------------------------------------------------------------
+let age = document.querySelector("#text1").innerText = dog.age;
 // console.log(age)
-//Get access to your location
-let cornfield = document.getElementById("location").innerText;
-console.log(cornfield)
-//Get access to your profileImg *
-let cardImg = document.querySelectorAll(".circle");
-console.log(cardImg)
+//Get access to your location ----------------------------------------------------------------------------------
+let cornfield = document.getElementById("location").innerText = dog.location;
+// console.log(cornfield)
+//Get access to your profileImg * ----------------------------------------------------------------------------
+let cardImg = document.querySelector(".circle").style.backgroundImage = dog.profileImg
+// console.log(cardImg)
 
-//     console.log(cardImg)
-
-//Get access to your bannerImg *
-let topImg = document.querySelectorAll(".top");
-console.log(topImg)
-//Get access to your backgroundColor *
-let background = document.querySelectorAll("body");
-console.log(background)
-//Get access to your stats *
-let standings = document.querySelector(".footer");
-console.log(standings)
+// //Get access to your bannerImg * -----------------------------------------------------------------------
+let topImg = document.querySelector(".top").style.backgroundImage = dog.bannerImg
+// console.log(topImg)
+// //Get access to your backgroundColor *----------------------------------------------------------------
+let background = document.querySelector("body").style.backgroundColor = dog.backgroundColor
+// console.log(background)
 
 
+// // //Get access to your stats * -----------------------------------------------------------
+// let standings1 = document.querySelector("#currentStats1").innerText = dog.stats.followers
+// // console.log(standings1)
+let standings2 = document.querySelector("#currentStats2").innerText = dog.stats.likes
+// console.log(standings2)
+let standings3 = document.querySelector("#currentStats3").innerText = dog.stats.posts 
+// console.log(standings3)
 
 
-// x = document.getElementsByTagName("*");
-// l = x.length;
-// for (i = 0; i < l; i++) {
-//   document.write(x[i].tagName + "<br>");
+// //----------------------------------------------------------------------
+//I need help assigning with a "for in" loop
+
+
+// for (const key in dog) {
+//     if (Object.hasOwnProperty.call(dog, key)) {
+//         const element = dog[key];
+//         if (element === dog.stats) {
+//             console.log(element)
+//              for (const key in element) {
+//                  if (Object.hasOwnProperty.call(element, key)) {
+//                      const dogStats = element[key];
+//                      if (dogStats == "1.2M") {
+//                          console.log(dogStats)
+//                         standings1 = dogStats
+//                      }
+                     
+//                  }
+//              }
+//         }
+//     }
 // }
 
+
+
+// // x = document.getElementsByTagName("*");
+// // l = x.length;
+// // for (i = 0; i < l; i++) {
+// //   document.write(x[i].tagName + "<br>");
+// // }
